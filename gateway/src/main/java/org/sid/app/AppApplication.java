6b5f6558af066e727877f2850d58a6c27bc3faa7
picojazz <picojazzz@gmail.com>
2021-02-16 14:30:51 +0000

    add a gateway and eureka discovery

---
+++ gateway/src/main/java/org/sid/app/AppApplication.java
@@ -0,0 +1,33 @@
+package org.sid.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class AppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
+
+    /*@Bean
+    RouteLocator staticRoutes(RouteLocatorBuilder routeLocatorBuilder){
+        return routeLocatorBuilder.routes()
+                //.route(r->r.path("/customers/**").uri("http://localhost:8081").id("r1"))
+                //.route(r->r.path("/products/**").uri("http://localhost:8082").id("r2"))
+                .route(r->r.path("/customers/**").uri("lb://CUSTOMER-SERVICE").id("r1"))
+                .route(r->r.path("/products/**").uri("lb://INVENTORY-SERVICE").id("r2"))
+                .build();
+    }*/
+    @Bean
+    DiscoveryClientRouteDefinitionLocator dynamicRoutes(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp){
+        return new DiscoveryClientRouteDefinitionLocator(rdc,dlp);
+    }
+}
+
